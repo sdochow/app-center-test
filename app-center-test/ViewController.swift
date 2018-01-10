1b9a3805529e0fa0a82ac69094c8b1b4eae9d26e
Sebastian Dochow <sebastian.dochow@gmail.com>
2018-01-10 21:03:39 +0100

    Initial Commit

---
+++ app-center-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-center-test
+//
+//  Created by Sebastian Dochow on 10.01.18.
+//  Copyright © 2018 prs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
